cb4ec840c641943dea2e299e8c582d427e8a8680
BigdaddyBryan <597912@edu.rocmn.nl>
2026-01-23 12:34:03 +0100

    Clean up whitespace in check.php for improved code readability

--- gdvoetbalschoen/phpcode/check.php
+++ gdvoetbalschoen/phpcode/check.php
@@ -20,14 +20,14 @@ echo "<h2>Juiste URLs voor jouw setup:</h2>";
 // Test of registercode.php bestaat
 if (file_exists(__DIR__ . '/registercode.php')) {
     echo "<p style='color: green;'>✓ registercode.php gevonden in deze directory</p>";
-    
+
     // Bereken de juiste URL
     $scriptPath = str_replace($_SERVER['DOCUMENT_ROOT'], '', __DIR__);
     $scriptPath = str_replace('\\', '/', $scriptPath);
-    
+
     echo "<p><strong>Test registratie URL:</strong><br>";
     echo "<a href='http://{$_SERVER['SERVER_NAME']}{$scriptPath}/test_register.php' style='color: blue; font-size: 18px;'>http://{$_SERVER['SERVER_NAME']}{$scriptPath}/test_register.php</a></p>";
-    
+
     echo "<p><strong>Register pagina URL:</strong><br>";
     $viewsPath = str_replace('phpcode', 'views', $scriptPath);
     echo "<a href='http://{$_SERVER['SERVER_NAME']}{$viewsPath}/register.php' style='color: blue; font-size: 18px;'>http://{$_SERVER['SERVER_NAME']}{$viewsPath}/register.php</a></p>";
@@ -41,13 +41,11 @@ try {
     require_once 'db_connection.php';
     $conn = getDbConnection();
     echo "<p style='color: green;'>✓ Database connectie OK</p>";
-    
+
     // Check users tabel
     $stmt = $conn->query("SELECT COUNT(*) as count FROM users");
     $result = $stmt->fetch();
     echo "<p>Aantal gebruikers in database: <strong>{$result['count']}</strong></p>";
-    
 } catch (Exception $e) {
     echo "<p style='color: red;'>✗ Database fout: " . $e->getMessage() . "</p>";
 }
-?>
