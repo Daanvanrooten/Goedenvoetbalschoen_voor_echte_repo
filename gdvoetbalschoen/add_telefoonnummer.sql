8f4c915c54c78e9fb4882a92c559ace72bb4ade6
BigdaddyBryan <597912@edu.rocmn.nl>
2026-01-26 12:30:01 +0100

    Remove deprecated email setup, SQL scripts, and debugging files to streamline the codebase

--- gdvoetbalschoen/add_telefoonnummer.sql
+++
@@ -1,4 +0,0 @@
--- Voeg telefoonnummer kolom toe aan users tabel
-ALTER TABLE `users` 
-ADD COLUMN `telefoonnummer` varchar(20) NOT NULL 
-AFTER `email`;
--- gdvoetbalschoen/add_telefoonnummer_safe.sql
+++
@@ -1,4 +0,0 @@
--- Voeg telefoonnummer kolom toe met default waarde voor bestaande users
-ALTER TABLE `users` 
-ADD COLUMN `telefoonnummer` varchar(20) NOT NULL DEFAULT '' 
-AFTER `email`;
