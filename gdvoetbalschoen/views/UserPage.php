79ba76a50c8c2a6704678a337a3f7396b162adad
KaiMarkies <593781@edu.rocmn.nl>
2026-01-29 09:49:05 +0100

    get all information from selected tasks
    +added some new styling

--- gdvoetbalschoen/views/UserPage.php
+++ gdvoetbalschoen/views/UserPage.php
@@ -10,26 +10,40 @@
     }
     $user = $_SESSION['user'];
     $UserID = $user['id'];
-
-    $sqla = "SELECT slot_id FROM task_registrations WHERE user_id = :user_id LIMIT 1";
-
-    $stmta = $conn->prepare($sqla);
-    $stmta->execute([':user_id' => $UserID]);
-    $Getregistrations = $stmta->fetch(PDO::FETCH_ASSOC);
-    $slotID = (int)$Getregistrations['slot_id'];
-
+    
+    $sqla = "SELECT slot_id FROM task_registrations WHERE user_id = :user_id";
     $sqlb = "SELECT task_id FROM task_slots WHERE slot_id = :slot_id LIMIT 1";
-
-    $stmtb = $conn->prepare($sqlb);
-    $stmtb->execute([':slot_id' => $slotID]);
-    $GetSlots = $stmtb->fetch(PDO::FETCH_ASSOC);
-    $taskID = (int)$GetSlots['task_id'];
-
+    $sqlc = "SELECT * FROM task_categories WHERE category_id = :category_id";
     $sql = "SELECT * FROM tasks WHERE task_id = :task_id";
 
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([':task_id' => $taskID]);
-    $AllTasks = $stmt->fetchall(PDO::FETCH_ASSOC);
+    //get slot_id(s) from registration to call task_id
+    $stmta = $conn->prepare($sqla);
+    $stmta->execute([':user_id' => $UserID]);
+    $Getregistrations = $stmta->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach($Getregistrations as $slotID){
+        
+        //get task_id here so can call tasks
+        $stmtb = $conn->prepare($sqlb);
+        $stmtb->execute([':slot_id' => $slotID["slot_id"]]);
+        $GetSlots = $stmtb->fetchAll(PDO::FETCH_ASSOC);
+        foreach($GetSlots as $taskID){
+            
+            //get tasks with TaskID
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([':task_id' => $taskID["task_id"]]);
+            $GetTask = $stmt->fetchall(PDO::FETCH_ASSOC);
+            $AllTasks[] = $GetTask;
+            
+            //get the category names
+            foreach($GetTask as $task){
+                $stmtc = $conn->prepare($sqlc);
+                $stmtc->execute([':category_id' => $task["category_id"]]);
+                $GetCategories = $stmtc->fetch(PDO::FETCH_ASSOC);
+                $Categories[] = $GetCategories["name"];
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -38,7 +52,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../assets/css/agenda.css">
+    <link rel="stylesheet" href="../css/UserPage.css">
 </head>
 <body>
     <header>
@@ -67,14 +81,44 @@
             </nav>
         </div>
     </header>
-
     <div class="MainArea">
+        <!-- TaskArea is voor Task display -->
+        <div class="TaskArea">
         <?php
-            foreach($AllTasks as $Task){
-                echo $Task["title"]; echo "<br>";
+            //get the taskcontainers from the array
+            foreach($AllTasks as $GetTask){
+                //get singular task information
+                foreach($GetTask as $task){
+                    if($task["frequency"] == null){
+                        $task["frequency"] = "ONCE";
+                    }
+                    echo "<div class='TaskBox'>";
+                        //Title and Description
+                        echo "<div class='descriptionArea'>";
+                        echo "<h3>" . htmlspecialchars($task["title"]) . "</h3>";
+                        echo "<div class='description'>" . htmlspecialchars($task["description"]) . "</div>";
+                        echo "</div>";
+                        //Times and dates
+                        echo "<div class='TimeArea'>";
+                        echo "<div>date: " . $task["day"] . "-" . $task["month"] . "-" . $task["year"]  . "</div>";
+                        echo "<div>Starts: " . $task["start_time"] . "</div>";
+                        echo "<div>Ends: " . $task["end_time"] . "</div>";
+                        echo "</div>";
+                        //Frequency and Category
+                        echo "<div class='OthersArea'>";
+                        echo "<div>Frequency: " . htmlspecialchars($task["frequency"]) . "</div>";
+                        echo "<div>Task Category: " . htmlspecialchars($Categories[0]) . "</div>";  // Changed this too
+                        echo "</div>";
+                    echo "</div>";
+                }
             }
         ?>
 
+            
+        </div>
+        <a href="logout.php">Log out</a>
     </div>
+    
+    
 </body>
 </html>
